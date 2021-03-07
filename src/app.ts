270ef2bc904e9f33a18bea5689e740a40801787b
David Whiting <davw@apache.org>
2021-03-07 15:12:33 +0100

    An artistic take

--- src/app.ts
+++ src/app.ts
@@ -42,25 +42,68 @@ function Gfx(canvas: HTMLCanvasElement) {
     function fade() {
         flag = -flag;
         g.save();
+        g.globalCompositeOperation = "source-over";
+        const gradient = g.createLinearGradient(0,0,0,h);
+        gradient.addColorStop(0.0, "#111144");
+        gradient.addColorStop(0.3, "#551144")
+        gradient.addColorStop(0.5, "#884444");
+        gradient.addColorStop(0.53, "#114477");
+        gradient.addColorStop(1.0, "#111144");
+
+        g.fillStyle = gradient;
+        g.globalAlpha = 0.1;
+        g.fillRect(0,0,w,h);
         gb.drawImage(canvas, 0,0);
 
+        // g.globalCompositeOperation = "source-over";
+        // g.fillStyle = "black";
+        // g.fillRect(0,0,w,h);
         g.globalCompositeOperation = "source-over";
-        g.fillStyle = "black";
-        g.fillRect(0,0,w,h);
-        g.globalCompositeOperation = "difference";
-        g.globalAlpha =1;
-        //g.filter = "blur(2px)";
-        g.drawImage(bb, 5,0);
-        g.drawImage(bb, -5,0);
+        g.globalAlpha = 0.95;
+        g.filter = "blur(5px)";
+        g.drawImage(bb, 0,0);
+
+        //g.drawImage(bb, -5,0);
 
 
         g.restore();
     }
 
+    function topEffect() {
+        g.globalCompositeOperation = "source-over";
+        g.lineWidth = 1;
+        for (let i =0 ; i < 7000; i++) {
+            let x=  Math.random() * w;
+            let y = Math.random() * h;
+            g.beginPath();
+            g.moveTo(x,y);
+            g.lineTo(x + Math.random()* 4 - 2,y+6);
+            let col = Math.floor(Math.random() * 255);
+            g.strokeStyle = "rgba(" + col + "," + col + "," + col + ",0.1)";
+            g.stroke();
+        }
+
+        const vignette = g.createRadialGradient(w/4,h/4,0,w/4,h/4,w);
+        vignette.addColorStop(0, "rgba(0,0,0,0.00)");
+        // vignette.addColorStop(0.15,"rgba(0,0,0,0)");
+        // vignette.addColorStop(0.85,"rgba(0,0,0,0)");
+        vignette.addColorStop(1.0, "rgba(0,0,0,0.2)");
+
+        g.fillStyle = vignette;
+        g.fillRect(0,0,w,h);
+
+        g.fillStyle = "rgba(255,255,192,0.2)"
+        g.beginPath();
+        g.arc(w/4,h/4,30,0,Math.PI *2);
+        g.fill();
+
+    }
+
     return {
         clear,
         drawNote,
-        fade
+        fade,
+        topEffect
     }
 }
 
@@ -120,7 +163,9 @@ function start() {
             gfx.fade();
         }
         notes.forEach(n => gfx.drawNote(n));
-
+        if (Math.floor(lastMs / 100) != Math.floor(ms / 100)) {
+            gfx.topEffect();
+        }
         window.requestAnimationFrame(frame);
         lastMs = ms;
     }
